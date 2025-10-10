d84cdbdd05c06bf430837e6ca53424952f7d25da
Wensi Ai <wsai@stanford.edu>
2025-10-10 14:18:32 -0700

    fix install / import errors

--- OmniGibson/omnigibson/objects/__init__.py
+++ OmniGibson/omnigibson/objects/__init__.py
@@ -1,5 +1,5 @@
 from omnigibson.objects.controllable_object import ControllableObject
-from omnigibson.objects.dataset_object import DatasetObject, DatasetType
+from omnigibson.objects.dataset_object import DatasetObject
 from omnigibson.objects.light_object import LightObject
 from omnigibson.objects.object_base import REGISTERED_OBJECTS, BaseObject
 from omnigibson.objects.primitive_object import PrimitiveObject
