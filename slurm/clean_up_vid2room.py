0c9f02843b8c1ad76b542df0ada31ef089d24fbd
Cem Gokmen <cgokmen@meta.com>
2026-01-30 12:15:02 +0000

    Cleanup file

--- slurm/clean_up_vid2room.py
+++ slurm/clean_up_vid2room.py
@@ -5,7 +5,7 @@ from concurrent.futures import ProcessPoolExecutor, as_completed
 def check_dir(obj_dir: pathlib.Path):
     success_file = obj_dir / "import.success"
     if not success_file.exists():
-        print(f"Success file not found for {obj_dir}")
+        # print(f"Success file not found for {obj_dir}")
         return False
     return True
 
@@ -13,6 +13,7 @@ def main():
     dataset_root = pathlib.Path("/checkpoint/clear/cgokmen/behavior-data2") / "vid2room"
 
     with ProcessPoolExecutor(max_workers=10) as executor:
+        bad_files = 0
         futures = {}
         scenes_dir = dataset_root / "scenes"
         for scene_dir in scenes_dir.iterdir():
@@ -26,7 +27,10 @@ def main():
                 futures[future] = obj_dir
 
         for future in tqdm(as_completed(futures), total=len(futures), desc="Checking directories"):
-            future.result()
+            if not future.result():
+                bad_files += 1
+
+        print(f"Found {bad_files} bad files out of {len(futures)} total files")
 
 if __name__ == "__main__":
     main()
