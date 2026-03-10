7100ab4e7eec1628776a28462bac5b9391e32957
Stef Ren <stefren97@gmail.com>
2026-03-10 15:20:32 -0700

    rm more instances

--- OmniGibson/omnigibson/objects/stateful_object.py
+++
@@ -1,5 +0,0 @@
-# StatefulObject has been merged into BaseObject.
-# This shim exists only for backwards compatibility with any external code that imports StatefulObject.
-from omnigibson.objects.object_base import BaseObject as StatefulObject
-
-__all__ = ["StatefulObject"]
