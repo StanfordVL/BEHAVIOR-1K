542141f96d8cedb2daa652641613bbfe36c54a5e
Wensi Ai <wsai@stanford.edu>
2026-04-07 14:26:29 -0700

    Revert robot asset download

--- OmniGibson/omnigibson/utils/asset_utils.py
+++ OmniGibson/omnigibson/utils/asset_utils.py
@@ -471,41 +471,12 @@ def download_and_unpack_zipped_dataset(dataset_name):
 
 def download_omnigibson_robot_assets():
     """
-    Clone OmniGibson robot assets from HuggingFace
+    Download OmniGibson assets
     """
-    dataset_path = get_dataset_path("omnigibson-robot-assets")
-    git_url = "https://huggingface.co/datasets/behavior-1k/omnigibson-robot-assets"
-
-    if os.path.exists(os.path.join(dataset_path, ".git")):
-        print("OmniGibson robot assets already cloned, pulling latest changes...")
-        subprocess.run(["git", "pull"], cwd=dataset_path, check=True)
-        _handle_lfs_pull(dataset_path)
-    elif os.path.exists(dataset_path):
-        print("Directory exists but is not a git repo. Removing and re-cloning...")
-        shutil.rmtree(dataset_path)
-        _clone_robot_assets(dataset_path, git_url)
+    if os.path.exists(get_dataset_path("omnigibson-robot-assets")):
+        print("Assets already downloaded.")
     else:
-        _clone_robot_assets(dataset_path, git_url)
-
-
-def _clone_robot_assets(dataset_path, git_url):
-    """Clone the robot assets repository"""
-    print(f"Cloning OmniGibson robot assets from {git_url}...")
-    subprocess.run(["git", "clone", git_url, dataset_path], check=True)
-    _handle_lfs_pull(dataset_path)
-
-
-def _handle_lfs_pull(dataset_path):
-    """Handle git lfs pull if applicable"""
-    gitattributes_path = os.path.join(dataset_path, ".gitattributes")
-    if os.path.exists(gitattributes_path):
-        with open(gitattributes_path, "r") as f:
-            if "lfs" in f.read():
-                print("Detected LFS files, running git lfs pull...")
-                try:
-                    subprocess.run(["git", "lfs", "pull"], cwd=dataset_path, check=True)
-                except subprocess.CalledProcessError:
-                    print("Warning: git lfs pull failed. If files are missing, make sure git-lfs is installed.")
+        download_and_unpack_zipped_dataset("omnigibson-robot-assets")
 
 
 def print_user_agreement():
