475c9f59acd8b8f328973a5c910388552225ad0f
Yalcin Tur <yalcinturpg@gmail.com>
2026-04-09 11:31:43 -0700

    Fix remaining assisted grasp timing with physics step

--- OmniGibson/omnigibson/robots/robot.py
+++ OmniGibson/omnigibson/robots/robot.py
@@ -1902,7 +1902,7 @@ class Robot(USDObject, GymObservable):
         assert self.is_manipulation
         if self._ag_obj_constraints[arm] is not None:
             self._release_grasp(arm=arm)
-            self._ag_release_counter[arm] = int(math.ceil(m.RELEASE_WINDOW / og.sim.get_sim_step_dt()))
+            self._ag_release_counter[arm] = int(math.ceil(m.RELEASE_WINDOW / og.sim.get_physics_dt()))
             self._handle_release_window(arm=arm)
             assert not self._ag_obj_in_hand[arm], "Object still in ag list after release!"
 
@@ -3112,7 +3112,7 @@ class Robot(USDObject, GymObservable):
         assert self.is_manipulation
         arm = self.default_arm if arm == "default" else arm
         self._ag_release_counter[arm] += 1
-        time_since_release = self._ag_release_counter[arm] * og.sim.get_sim_step_dt()
+        time_since_release = self._ag_release_counter[arm] * og.sim.get_physics_dt()
         if time_since_release >= m.RELEASE_WINDOW:
             self._ag_obj_in_hand[arm] = None
             self._ag_release_counter[arm] = None
