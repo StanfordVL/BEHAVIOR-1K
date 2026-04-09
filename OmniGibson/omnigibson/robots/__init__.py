6364deb7c53d25bd217916128dc5b31eddd8fc73
Wensi Ai <wsai@stanford.edu>
2026-04-09 13:34:53 -0700

    Update robot definition and assets handling

--- OmniGibson/omnigibson/robots/__init__.py
+++ OmniGibson/omnigibson/robots/__init__.py
@@ -1,10 +1,12 @@
 from pathlib import Path
 from omnigibson.robots.robot import Robot
+from omnigibson.utils.asset_utils import get_dataset_path
 
 REGISTERED_ROBOTS = []
-robot_config_dir = Path(__file__).parent / "definitions"
-for yaml_file in sorted(robot_config_dir.glob("*.yaml")):
-    REGISTERED_ROBOTS.append(yaml_file.stem)
+robot_config_dir = Path(get_dataset_path("omnigibson-robot-assets")) / "models"
+for yaml_file in sorted(robot_config_dir.glob("*/*.yaml")):
+    if yaml_file.stem == yaml_file.parent.name:
+        REGISTERED_ROBOTS.append(yaml_file.stem)
 
 __all__ = [
     "Robot",
