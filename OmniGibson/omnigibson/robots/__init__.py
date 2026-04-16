93abb46b4285f1ede9613da3e7e8c42997247277
Wensi Ai <wsai@stanford.edu>
2026-04-16 16:47:29 -0700

    Make REGISTERED_ROBOTS support arbitrary assets in datasets folder

--- OmniGibson/omnigibson/robots/__init__.py
+++ OmniGibson/omnigibson/robots/__init__.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from omnigibson.robots.robot import Robot
-from omnigibson.utils.asset_utils import get_dataset_path
+from omnigibson.macros import gm
+
 
 REGISTERED_ROBOTS = []
-robot_config_dir = Path(get_dataset_path("omnigibson-robot-assets")) / "models"
-for yaml_file in sorted(robot_config_dir.glob("*/*.yaml")):
+for yaml_file in sorted(Path(gm.DATA_PATH).glob("*/models/*/*.yaml")):
     if yaml_file.stem == yaml_file.parent.name:
         REGISTERED_ROBOTS.append(yaml_file.stem)
 
