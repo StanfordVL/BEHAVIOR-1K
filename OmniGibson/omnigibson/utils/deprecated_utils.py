cbd13c78cb40f41c0ce33867b8af74b95e14b72c
Andorexad <andixu@umich.edu>
2026-06-05 17:33:40 -0700

    merged tiled rendering

--- OmniGibson/omnigibson/utils/deprecated_utils.py
+++ OmniGibson/omnigibson/utils/deprecated_utils.py
@@ -3,7 +3,7 @@ A set of utility functions slated to be deprecated once Omniverse bugs are fixed
 """
 
 import math
-from typing import List, Optional, Tuple, Union
+from typing import Any, List, Optional, Tuple, Union
 
 import carb
 import numpy as np
@@ -1055,3 +1055,59 @@ def colorize_bboxes(bboxes_2d_data, bboxes_2d_rgb, num_channels=3):
         rgb_img_draw.rectangle([(bbox_2d[1], bbox_2d[2]), (bbox_2d[3], bbox_2d[4])], outline=outline, width=2)
     bboxes_2d_rgb = np.array(rgb_img)
     return bboxes_2d_rgb
+
+
+@wp.kernel(enable_backward=False)
+def reshape_tiled_image(
+    tiled_image_buffer: Any,
+    batched_image: Any,
+    image_height: int,
+    image_width: int,
+    num_channels: int,
+    num_tiles_x: int,
+):
+    """Reshapes a tiled image into a batch of images.
+    This function reshapes the input tiled image buffer into a batch of images. The input image buffer
+    is assumed to be tiled in the x and y directions. The output image is a batch of images with the
+    specified height, width, and number of channels.
+    Args:
+        tiled_image_buffer: The input image buffer. Shape is (height * width * num_channels * num_cameras,).
+        batched_image: The output image. Shape is (num_cameras, height, width, num_channels).
+        image_width: The width of the image.
+        image_height: The height of the image.
+        num_channels: The number of channels in the image.
+        num_tiles_x: The number of tiles in x-direction.
+    """
+    # get the thread id
+    camera_id, height_id, width_id = wp.tid()
+
+    # resolve the tile indices
+    tile_x_id = camera_id % num_tiles_x
+    tile_y_id = camera_id // num_tiles_x
+    # compute the start index of the pixel in the tiled image buffer
+    pixel_start = (
+        num_channels * num_tiles_x * image_width * (image_height * tile_y_id + height_id)
+        + num_channels * tile_x_id * image_width
+        + num_channels * width_id
+    )
+
+    # copy the pixel values into the batched image
+    for i in range(num_channels):
+        batched_image[camera_id, height_id, width_id, i] = tiled_image_buffer[pixel_start + i]
+
+
+# uint32 -> int32 conversion is required for non-colored segmentation annotators
+wp.overload(
+    reshape_tiled_image,
+    {"tiled_image_buffer": wp.array(dtype=wp.uint32), "batched_image": wp.array(dtype=wp.uint32, ndim=4)},
+)
+# uint8 is used for 4 channel annotators
+wp.overload(
+    reshape_tiled_image,
+    {"tiled_image_buffer": wp.array(dtype=wp.uint8), "batched_image": wp.array(dtype=wp.uint8, ndim=4)},
+)
+# float32 is used for single channel annotators
+wp.overload(
+    reshape_tiled_image,
+    {"tiled_image_buffer": wp.array(dtype=wp.float32), "batched_image": wp.array(dtype=wp.float32, ndim=4)},
+)
